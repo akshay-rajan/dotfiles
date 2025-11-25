48a5f0abe33d295357c26b556cce7967d9b7aef3
Akshay R <145046584+akshay-rajan@users.noreply.github.com>
2025-11-25 21:59:43 +0530

    Set line-by-line screen movement to Alt+j and Alt+k

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -19,3 +19,7 @@ vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<A-j>", "<C-e>", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-k>", "<C-y>", { noremap = true, silent = true })
+
